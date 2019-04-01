18a08a3a972d0e8ac613bbcc70df5a061a92ac0c
Frederik Aarup Lauridsen <frederik.lauridsen@gmail.com>
2019-04-01 16:53:21 +0200

    initial

---
+++ Assets/CSMeshLive.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSMeshLive : MonoBehaviour
+{
+
+    public struct Triangle{ // order and size important. can be done with arrays if necessary, and some properties can be enforced with flags.
+        public Vector3 center;
+        public Vector3 normal;
+    }
+
+    public ComputeShader shader;
+
+    public ComputeBuffer trianglesBuffer; // send and receive data using this buffer
+    public Triangle[] triangles;
+    
+
+    void Start()
+    {
+        int kernelId = shader.FindKernel("CSTriangles");
+
+        trianglesBuffer = new ComputeBuffer(128, sizeof(float) * 6); // same as array, data types * number of them (3 floats per vector, two vectors, each float is 4 bytes = 24)
+        triangles = new Triangle[128]; // same as number of threads
+
+        shader.SetBuffer(kernelId, "ResultTriangles"/* name or id, id more efficient, shader.PropertyToId*/, trianglesBuffer);
+        shader.Dispatch(kernelId, 2, 1, 1);
+
+        trianglesBuffer.GetData(triangles); // blocking, waiting for data
+
+        
+        
+    }
+
+    private void OnDrawGizmos(){
+        if (triangles != null){
+            foreach (Triangle triangle in triangles){
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(triangle.center, triangle.center + triangle.normal);
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(triangle.center, 0.01f);
+            }
+        }
+    }
+
+}
---
+++ Assets/computeshaderlive.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class computeshaderlive : MonoBehaviour
+{
+    public ComputeShader cs;
+    public Texture2D inputTexture;
+
+    private RenderTexture outputTexture;
+
+    private Matrix4x4 colorTransform = new Matrix4x4(
+                                                     new Vector4(1, 0, 0, 0),
+                                                     new Vector4(0, 1, 0, 0),
+                                                     new Vector4(0, 0, 1, 0),
+                                                     new Vector4(0, 0, 0, 1)
+                                                     ); // different matrices can be used to do grayscale or sepia or other color schemes
+
+
+    void Start()
+    {
+        int kernelID = cs.FindKernel("CSMain"); // get ID of kernel, first pragma line
+        int inTextureID = Shader.PropertyToID("InputTexture");
+        int textureID = Shader.PropertyToID("Result");  // get ID of texture, name in shader
+        int colorMatId = Shader.PropertyToID("ColorTransform");
+
+        outputTexture = new RenderTexture(inputTexture.width, inputTexture.height, 0, RenderTextureFormat.ARGB32);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.filterMode = FilterMode.Point; // Create sharp edges to see pixels better
+        outputTexture.Create();  // Reanable after changing settings
+
+        cs.SetTexture(kernelID, inTextureID, inputTexture);
+
+        cs.SetTexture(kernelID, textureID, outputTexture);
+        cs.SetMatrix(colorMatId, colorTransform);
+        
+        cs.Dispatch(kernelID, outputTexture.width/8, outputTexture.height/8, 1);  // texture size divided by threads on that axis gives number of groups in each axis
+
+        Material material = new Material(Shader.Find("Unlit/Texture"));
+        material.mainTexture = outputTexture;
+        GetComponent<MeshRenderer>().sharedMaterial = material;
+
+    }
+
+
+}
